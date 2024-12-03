ce14c3298844550f16bb47d95dc063299846c8ca
baller4ever11 <rucogoza.miguel@cheverus.org>
2024-12-03 08:42:43 -0500

    cc

--- constants.py
+++ constants.py
@@ -1,8 +1,23 @@
 import pygame as pg
+import pygame_widgets
+from ply.yacc import restart
 
 # MISC
 SCREENSIZE = (400,400)
 BACKGROUNDCOLOR = "dark gray"
 FRAMERATE = 30
 
+#Commands
+def restart_game():
 
+
+
+
+#Buttons
+restart_button = tk.Button(root, text="Restart Game", command=restart_game)
+
+
+
+
+#Pack
+restart_button_button.pack(side=tk.TOP)
